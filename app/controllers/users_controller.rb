cfac89448d639d619e8b921452cd9e3a44130ce6
Danial-11 <danial.azeem@devsinc.com>
2022-04-28 13:25:49 +0500

    Creating post using cloudinary without gui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-
+    def index
+        @users = User.all
+    end
+    
     def homepage
         if !user_signed_in?
             redirect_to new_user_session_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root "users#homepage"
+  get 'search', to: "users#search"
   devise_for :users
+  resources :users
+  resources :posts, only: [:index, :show, :create] do
+    resources :photos, only: [:create]
+  end
 end
